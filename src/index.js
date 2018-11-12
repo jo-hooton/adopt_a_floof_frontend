0fe4e246a3f2194e1ce62151edc565cd4902ff78
Autonomoustic <aa_connect@outlook.com>
2018-11-12 08:41:37 +0000

    css

--- src/index.js
+++ src/index.js
@@ -7,162 +7,215 @@ const signInForm = document.querySelector('#sign-up')
 signInForm.addEventListener('submit', (e) => signIn(e))
 const signOutButton = document.querySelector('#sign-out-btn')
 let addPet = false
-
-
+currentUser = parseInt(localStorage.getItem('user-id'))
 
 const signIn = (e) => {
-    e.preventDefault()
+  e.preventDefault()
 
-    const userNameInput = document.querySelector('#username')
-    const userPostcode = document.querySelector('#postcode')
+  const userNameInput = document.querySelector('#username')
+  const userPostcode = document.querySelector('#postcode')
 
-    const newUser = {
-        name: userNameInput.value,
-        postcode: userPostcode.value,
-    }
-    //post request to add new user
-    fetch('http://localhost:3000/api/v1/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newUser)
-        })
+  const newUser = {
+    name: userNameInput.value,
+    postcode: userPostcode.value
+  }
+    // post request to add new user
+  fetch('http://localhost:3000/api/v1/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUser)
+  })
         .then(resp => resp.json())
         .then(user => {
-            if(!user.error){
-                signInForm.style.display = 'none'
-                signOutButton.style.display = 'block'
-                localStorage.setItem('user-id', user.id)
-            }
-    })
-
+          if (!user.error) {
+            signInForm.style.display = 'none'
+            signOutButton.style.display = 'block'
+            localStorage.setItem('user-id', user.id)
+            // add show favourites button
+            favouritesButton()
+            // add event listner to button
+          }
+        })
 }
-//render pet listings
+
+function favouritesButton () {
+  const userFavourites = document.createElement('button')
+  const nav = document.querySelector('#navBar')
+  userFavourites.innerText = 'Favourites'
+  userFavourites.id = 'userFav'
+  nav.appendChild(userFavourites)
+  FavButtonEventHandler()
+}
+
+function FavButtonEventHandler () {
+  const favButton = document.querySelector('#userFav')
+  favButton.addEventListener('click', event => {
+    console.log(event)
+    petListings.innerHTML = ''
+    // find out whos logged in
+
+    // get wishlist of user
+    getWishlistOfUser()
+    // a  ppend pets details to pet listing
+  })
+}
+
+function getWishlistOfUser () {
+  return fetch('http://localhost:3000/api/v1/wishlists')
+    .then(resp => resp.json())
+    .then(wishlists => {
+      wishlists.find(wishlist => {
+        if (wishlist.user_id === currentUser) {
+          console.log(wishlist.pets)
+          wishlist.pets.forEach(pet => {
+            createPet(pet)
+          })
+        }
+      })
+    })
+}
+
+// render pet listings
 const createPet = pet => {
-    const petEl = `
+  const petEl = `
     <div id="${pet.id}"class="card">
       <h2>${pet.name}</h2>
-      <img src=${pet.img_url} class="pet-img">
-      <p>Species: ${pet.species}<p>
-      <p>Breed: ${pet.breed}<p>
-      <p>Age: ${pet.years} Years, ${pet.months} Months<p>
-      <p>Temperament: ${pet.temperament}<p>
-      <p>Description: ${pet.description}<p>
-      <button id="${pet.id}-wishlist" class="wishlist-btn">Add to favourites</button>
+      <div style="background-image: url(${pet.img_url})" class="pet-img"></div>
+      <p class="card-text">Species: ${pet.species}</p>
+      <p class="card-text">Breed: ${pet.breed}</p>
+      <p class="card-text">Age: ${pet.years} Years, ${pet.months} Months</p>
+      <p class="card-text">Temperament: ${pet.temperament}</p>
+      <p class="card-text">Description: ${pet.description}</p>
+      <br>
+      <button style="margin: 4px;" id="${pet.id}-wishlist" class="wishlist-btn">Add to favourites</button>
+      <button style="margin: 4px;" id="adopt-${pet.id}" class="adopt-btn">Adopt ${pet.name}</button>
     </div>
     `
-    
-    petListings.innerHTML += petEl
 
+  petListings.innerHTML += petEl
 
-    document.body.addEventListener('click', event => {
-        if (event.target.id === pet.id + '-wishlist') {
-            wishListAdd(pet)
-        }
-    })
+  isAdopted(pet)
+
+  document.body.addEventListener('click', event => {
+    if (event.target.id === pet.id + '-wishlist') {
+      wishListAdd(pet)
+    }
+  })
+  document.body.addEventListener('click', event => {
+    if (event.target.id === 'adopt-' + pet.id) {
+      adoptPet(pet)
+
+      const adoptBtn = document.querySelector(`#adopt-${pet.id}`)
+      adoptBtn.innerText = 'Happily rehomed'
+    }
+  })
 }
-
-
 
 // create new pet
 const submitPet = (e) => {
-    e.preventDefault()
+  e.preventDefault()
 
-    const newPet = {
-        'reserved': false,
-        'adopted': false,
-        user_id: localStorage.getItem('user-id')
-    }
+  const newPet = {
+    'reserved': false,
+    'adopted': false,
+    user_id: localStorage.getItem('user-id')
+  }
 
-    const petInputs = document.getElementsByClassName('input-text')
-    for (input of petInputs) {
-        newPet[input.name] = input.value
-    }
+  const petInputs = document.getElementsByClassName('input-text')
+  for (input of petInputs) {
+    newPet[input.name] = input.value
+  }
 
-    fetch('http://localhost:3000/api/v1/pets', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newPet)
-        }).then(resp => resp.json())
+  fetch('http://localhost:3000/api/v1/pets', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newPet)
+  }).then(resp => resp.json())
         .then(pet => createPet(pet))
 
-        createForm.reset()
-        createForm.style.display = 'none'
-
+  createForm.reset()
+  createForm.style.display = 'none'
 }
-
 
 // add to wishlist
 const wishListAdd = (pet) => {
-    
-    fetch('http://localhost:3000/api/v1/wishlists')
+  fetch('http://localhost:3000/api/v1/wishlists')
             .then(resp => resp.json())
             .then(wishlists => {
-                const currentUser = parseInt(localStorage.getItem('user-id'))
-                const currentWishlist = wishlists.find(wishlist => wishlist.user_id === currentUser)
-                if (currentWishlist) {
-                    currentWishlist.pets.push(pet.id)
-                    patchWishlistToServer(currentWishlist)
-                } else {
-                    console.log(pet.id)
-                    postWishlistToServer(currentUser)
-                } 
+              const currentWishlist = wishlists.find(wishlist => wishlist.user_id === currentUser)
+              if (currentWishlist) {
+                patchPetWishlist(pet, currentWishlist.id)
+              } else {
+                console.log(pet.id)
+                postWishlistToServer(currentUser)
+              }
             })
-
-    
 }
 
 function postWishlistToServer (user) {
-    fetch('http://localhost:3000/api/v1/wishlists', {
-        method: 'POST',
-        headers: { 'Content-Type' : 'application/json' },
-        body: JSON.stringify({user_id: user})
-    }).then(resp => resp.json())
-
+  fetch('http://localhost:3000/api/v1/wishlists', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({user_id: user})
+  }).then(resp => resp.json())
 }
 
-function patchWishlistToServer (wishlist, newFavourites) {
+function patchPetWishlist (pet, newWishlist) {
+  fetch(`http://localhost:3000/api/v1/pets/${pet.id}`, {
+    'method': 'PATCH',
+    'headers': { 'Content-Type': 'application/json' },
+    'body': JSON.stringify({wishlist_id: newWishlist})
+  }).then(resp => resp.json())
+}
 
-    fetch(`http://localhost:3000/api/v1/wishlists/${wishlist.id}`, {
-        'method': 'PATCH', 
-        'headers': { 'Content-Type' : 'application/json' },
-        'body': JSON.stringify({pets: newFavourites})
-    }).then(resp => resp.json())
+const adoptPet = (pet) => {
+  fetch(`http://localhost:3000/api/v1/pets/${pet.id}`, {
+    'method': 'PATCH',
+    'headers': { 'Content-Type': 'application/json' },
+    'body': JSON.stringify({user_id: currentUser, adopted: true})
+  })
 }
 
 addBtn.addEventListener('click', () => {
-    addPet = !addPet
-    if (addPet) {
-        petForm.style.display = 'block'
-    } else {
-        petForm.style.display = 'none'
-    }
+  addPet = !addPet
+  if (addPet) {
+    petForm.style.display = 'block'
+  } else {
+    petForm.style.display = 'none'
+  }
 })
 
 signOutButton.addEventListener('click', (event) => {
-    console.log(event)
-    localStorage.clear()
-    hideSignOut()
-    signInForm.style.display = 'block'
+  console.log(event)
+  localStorage.clear()
+  hideSignOut()
+  signInForm.style.display = 'block'
 })
 
 fetch('http://localhost:3000/api/v1/pets')
     .then(resp => resp.json())
     .then(pets => pets.forEach(pet => {
-        createPet(pet)
+      createPet(pet)
     }))
 
-// const hideSignIn = () => {
-//     if (localStorage === null) {
-//     }
-// }
-
-const hideSignOut = () => {
-    signOutButton.style.display = 'none'
+const hideSignIn = () => {
+  if (localStorage === null) {
+  }
 }
 
+const hideSignOut = () => {
+  signOutButton.style.display = 'none'
+}
+
+const isAdopted = (pet) => {
+  if (pet.adopted === true) {
+    const adoptBtn = document.querySelector(`#adopt-${pet.id}`)
+    adoptBtn.innerText = 'Happily rehomed'
+  }
+}
 
 hideSignOut()
